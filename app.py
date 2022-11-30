d83e28d05302950c315b3af60ed2f8dabf62140a
Aliviya93 <119508187+Aliviya93@users.noreply.github.com>
2022-11-30 20:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ st.write("TDS WEEK 8")
 A=st.number_input("Enter first number")
 B=st.number_input("Enter second number") 
 C=A-B
-st.write("Difference of given number is",c)
+st.write("Difference of given number is",C)
 st.write("By ALIVIYA BHOWMICK 21F1003575")
