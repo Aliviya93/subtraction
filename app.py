42914f1c05c7c348c9a468da1e1282e18b9572d2
Aliviya93 <119508187+Aliviya93@users.noreply.github.com>
2022-11-30 20:02:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.write("TDS WEEK 8")
+A=st.number_input("Enter first number")
+B=st.number_input("Enter second number") 
+C=A-B
+st.write("Difference of given number is",c)
+st.write("By ALIVIYA BHOWMICK 21F1003575")
